50ebe6ad9d58269e8fb8c8794107cd78433dd94c
Joshlanuevo <lanuevo68@gmail.com>
2024-07-01 17:58:33 +0800

    modify fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("io.coil-kt:coil:2.0.0")
 }
--- app/src/main/java/com/vancoding/tasksapp/ui/fragment/TasksFragment.kt
+++ app/src/main/java/com/vancoding/tasksapp/ui/fragment/TasksFragment.kt
@@ -1,16 +1,45 @@
 package com.vancoding.tasksapp.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import coil.load
 import com.vancoding.tasksapp.R
 import com.vancoding.tasksapp.databinding.FragmentTasksBinding
 import com.vancoding.tasksapp.mvvm.BaseFragment
 
-class TasksFragment: BaseFragment(R.layout.fragment_tasks) {
-    private lateinit var bindView: FragmentTasksBinding
+class TasksFragment : BaseFragment(R.layout.fragment_tasks) {
+    private var _binding: FragmentTasksBinding? = null
+    private val binding get() = _binding!!
 
-    override fun initView() {}
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTasksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-    override fun requestData() {}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
 
-    override fun observeCallBack() {}
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
+    override fun initView() {
+        binding.ivLogo.load(R.mipmap.ic_launcher)
+    }
+
+    override fun requestData() {
+        // Fetch data...
+    }
+
+    override fun observeCallBack() {
+        // Observe LiveData...
+    }
 }
