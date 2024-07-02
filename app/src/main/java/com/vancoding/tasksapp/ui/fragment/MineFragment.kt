6db8cff070e398c5dece8f31652f9fcb0e9c961b
Joshlanuevo <lanuevo68@gmail.com>
2024-07-02 14:14:40 +0800

    modify mine fragment

--- app/src/main/java/com/vancoding/tasksapp/ui/fragment/MineFragment.kt
+++ app/src/main/java/com/vancoding/tasksapp/ui/fragment/MineFragment.kt
@@ -1,15 +1,44 @@
 package com.vancoding.tasksapp.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import coil.load
 import com.vancoding.tasksapp.R
 import com.vancoding.tasksapp.databinding.FragmentMineBinding
 import com.vancoding.tasksapp.mvvm.BaseFragment
 
 class MineFragment: BaseFragment(R.layout.fragment_mine) {
-    private lateinit var bindView: FragmentMineBinding;
+    private  var _binding: FragmentMineBinding? = null;
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMineBinding.inflate(inflater, container, false);
+        return binding.root;
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     override fun initView() {}
 
-    override fun requestData() {}
+    override fun requestData() {
+        // Fetch data...
+    }
 
-    override fun observeCallBack() {}
+    override fun observeCallBack() {
+        // Observe LiveData...
+    }
 }
