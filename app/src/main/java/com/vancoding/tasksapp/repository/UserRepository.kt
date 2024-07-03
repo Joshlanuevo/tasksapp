857b77f48d5f93af5a0142f9bddca462e1a105d2
Joshlanuevo <lanuevo68@gmail.com>
2024-07-03 11:29:56 +0800

    add repository

---
+++ app/src/main/java/com/vancoding/tasksapp/repository/UserRepository.kt
@@ -0,0 +1,15 @@
+package com.vancoding.tasksapp.repository
+
+import com.vancoding.tasksapp.bean.UserBean
+import com.vancoding.tasksapp.db.UserDao
+
+class UserRepository(private val userDao: UserDao) {
+
+    suspend fun insert(user: UserBean): Long {
+        return userDao.insert(user);
+    }
+
+    suspend fun getUser(username: String, password: String): UserBean? {
+        return userDao.getUser(username, password)
+    }
+}
