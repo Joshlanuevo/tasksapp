7fc4467df66bfcffd9a634e57359c1db7b793657
Joshlanuevo <lanuevo68@gmail.com>
2024-07-03 12:34:53 +0800

    add register view model

--- app/src/main/java/com/vancoding/tasksapp/ui/RegisterActivity.kt
+++ app/src/main/java/com/vancoding/tasksapp/ui/RegisterActivity.kt
@@ -1,15 +1,38 @@
 package com.vancoding.tasksapp.ui
 
 import android.content.Intent
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.activity.viewModels
+import com.vancoding.tasksapp.bean.UserBean
 import com.vancoding.tasksapp.databinding.ActivityRegisterBinding
+import com.vancoding.tasksapp.db.UserDb
 import com.vancoding.tasksapp.mvvm.BaseActivity
+import com.vancoding.tasksapp.repository.UserRepository
+import com.vancoding.tasksapp.viewmodel.LoginViewModelFactory
+import com.vancoding.tasksapp.viewmodel.RegisterViewModel
+import com.vancoding.tasksapp.viewmodel.RegisterViewModelFactory
 
 class RegisterActivity : BaseActivity() {
     private lateinit var binding: ActivityRegisterBinding;
+    private val mViewModel: RegisterViewModel by viewModels {
+        RegisterViewModelFactory(UserRepository(UserDb.getDatabase(this).userDao))
+    }
 
     override fun initView() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            val username = binding.etUsername.text.toString().trim();
+            val password = binding.etPassword.text.toString().trim();
+
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter both username and password", Toast.LENGTH_SHORT).show()
+            } else {
+                mViewModel.insert(username, password)
+            }
+        }
 
         binding.BtnLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java);
@@ -19,5 +42,15 @@ class RegisterActivity : BaseActivity() {
 
     override fun requestData() {}
 
-    override fun observeCallBack() {}
+    override fun observeCallBack() {
+        mViewModel.user.observe(this, Observer { user ->
+            if (user != null) {
+                Toast.makeText(this, "Register Successful", Toast.LENGTH_SHORT).show();
+                startActivity(Intent(this, HomeActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
 }
--- app/src/main/java/com/vancoding/tasksapp/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/vancoding/tasksapp/viewmodel/RegisterViewModel.kt
@@ -8,14 +8,19 @@ import com.vancoding.tasksapp.bean.UserBean
 import com.vancoding.tasksapp.repository.UserRepository
 import kotlinx.coroutines.launch
 
-class LoginViewModel(private val repository: UserRepository): ViewModel() {
+class RegisterViewModel(private val repository: UserRepository): ViewModel() {
 
     private val _user = MutableLiveData<UserBean?>()
     val user: LiveData<UserBean?>
         get() = _user
 
-    fun getUser(username: String, password: String) = viewModelScope.launch {
-        val result = repository.getUser(username, password)
-        _user.postValue(result)
+    fun insert(username: String, password: String) = viewModelScope.launch {
+        val user = UserBean(username = username, password = password)
+        val userId = repository.insert(user)
+        if (userId > 0) {
+            _user.postValue(user)
+        } else {
+            _user.postValue(null)
+        }
     }
 }
