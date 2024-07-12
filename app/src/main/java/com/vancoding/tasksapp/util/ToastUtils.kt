6b83fafd2a6cd3b139db4dd00bf1e5184b218035
Joshlanuevo <lanuevo68@gmail.com>
2024-07-12 20:35:08 +0800

    update toast

--- app/src/main/java/com/vancoding/tasksapp/util/ToastUtils.kt
+++ app/src/main/java/com/vancoding/tasksapp/util/ToastUtils.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import android.widget.Toast
 import com.vancoding.tasksapp.R
@@ -12,19 +13,25 @@ object ToastUtils {
 
     fun showToast(context: Context, message: String, view: View) {
         // Hide the keyboard
-        KeyboardUtils.hideKeyboard(view)
+        hideKeyboard(context, view)
 
-        // Create and show the custom toast with default style
+        // Create and show the custom toast
         val inflater = LayoutInflater.from(context)
         val toastView = inflater.inflate(R.layout.layout_custom_toast, null)
 
         val textViewToast = toastView.findViewById<TextView>(R.id.textViewToast)
         textViewToast.text = message
 
-        val toast = Toast(context)
-        toast.duration = Toast.LENGTH_SHORT
-        toast.view = toastView
-        toast.setGravity(Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL, 0, 250)
-        toast.show()
+        Toast(context).apply {
+            setGravity(Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL, 0, 150)
+            duration = Toast.LENGTH_SHORT
+            setView(toastView)
+            show()
+        }
+    }
+
+    private fun hideKeyboard(context: Context, view: View) {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
